d264ea3435afbbf0ae17d8e9f93b13a4b6c8bc0c
Jan Ziemniewicz <jan.ziemniewicz@gmail.com>
2020-06-24 16:56:37 +0200

    Connection works till shipSetup

---
+++ battleship-front/src/app/dialogs/waiting-dialog/waiting-dialog.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Inject } from "@angular/core";
+import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+
+@Component({
+  selector: "app-waiting-dialog",
+  templateUrl: "./waiting-dialog.component.html",
+  styleUrls: ["./waiting-dialog.component.scss"],
+})
+export class WaitingDialogComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
